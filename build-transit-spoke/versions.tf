0a66bcb19b698a80c49f47bbbbcf4f0bd5c9d28d
Ronald Lee <rlee@aviatrix.com>
2022-02-28 12:45:35 -0800

    Format code

--- build-transit-spoke/versions.tf
+++ build-transit-spoke/versions.tf
@@ -4,7 +4,7 @@ terraform {
       source  = "AviatrixSystems/aviatrix"
       version = "2.21.0-6.6.ga"
     }
-        aws = {
+    aws = {
       source  = "hashicorp/aws"
       version = "4.2.0"
     }
